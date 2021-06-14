31b5da265e06d5b964bdb9f560ff8f0342c6f613
Roger Toledo <rogertoledo28@gmail.com>
2021-06-13 23:55:14 -0300

    Queue (#1)
    * Exercicios Queue

--- java/exercicios/src/main/java/me/rto/collections/Main.java
+++ java/exercicios/src/main/java/me/rto/collections/Main.java
@@ -2,7 +2,10 @@ package me.rto.collections;
 
 public class Main {
     public static void main(String[] args) {
-        CollectionsExerc collections = new CollectionsExerc();
-        collections.collections();
+        //CollectionsExerc collections = new CollectionsExerc();
+        //collections.collections();
+
+        QueueExerc queue = new QueueExerc();
+        queue.queue();
     }
 }
