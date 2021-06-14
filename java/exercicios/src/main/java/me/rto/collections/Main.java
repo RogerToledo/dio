e23323d92273b74e0f4e98378cb9ec973d35c992
roger.toledo <rogertoledo28@gmail.com>
2021-06-13 21:57:38 -0300

    Adiciona exercicios sobre collections

---
+++ java/exercicios/src/main/java/me/rto/collections/Main.java
@@ -0,0 +1,8 @@
+package me.rto.collections;
+
+public class Main {
+    public static void main(String[] args) {
+        CollectionsExerc collections = new CollectionsExerc();
+        collections.collections();
+    }
+}
